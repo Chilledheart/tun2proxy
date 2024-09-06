c32f3188960aedd5ca9634f0d7bd092699aef73e
Keeyou <keeyou-cn@outlook.com>
2024-09-06 13:52:17 +0800

    update dependencies

--- src/setup.rs
+++ src/setup.rs
@@ -9,7 +9,7 @@ use std::{
     fs,
     io::BufRead,
     net::{Ipv4Addr, Ipv6Addr},
-    os::unix::io::RawFd,
+    os::fd::{AsRawFd, BorrowedFd, IntoRawFd, RawFd},
     process::{Command, Output},
     str::FromStr,
 };
@@ -152,7 +152,7 @@ impl Setup {
         Ok(false)
     }
 
-    fn write_buffer_to_fd(fd: RawFd, data: &[u8]) -> Result<(), Error> {
+    fn write_buffer_to_fd(fd: BorrowedFd, data: &[u8]) -> Result<(), Error> {
         let mut written = 0;
         loop {
             if written >= data.len() {
@@ -163,10 +163,10 @@ impl Setup {
         Ok(())
     }
 
-    fn write_nameserver(fd: RawFd) -> Result<(), Error> {
+    fn write_nameserver(fd: BorrowedFd) -> Result<(), Error> {
         let data = "nameserver 198.18.0.1\n".as_bytes();
         Self::write_buffer_to_fd(fd, data)?;
-        nix::sys::stat::fchmod(fd, nix::sys::stat::Mode::from_bits(0o444).unwrap())?;
+        nix::sys::stat::fchmod(fd.as_raw_fd(), nix::sys::stat::Mode::from_bits(0o444).unwrap())?;
         Ok(())
     }
 
@@ -176,7 +176,7 @@ impl Setup {
             nix::fcntl::OFlag::O_RDWR | nix::fcntl::OFlag::O_CLOEXEC | nix::fcntl::OFlag::O_CREAT,
             nix::sys::stat::Mode::from_bits(0o644).unwrap(),
         )?;
-        Self::write_nameserver(fd)?;
+        Self::write_nameserver(unsafe { BorrowedFd::borrow_raw(fd) })?;
         let source = format!("/proc/self/fd/{}", fd);
         if Ok(())
             != nix::mount::mount(
@@ -197,7 +197,7 @@ impl Setup {
                 nix::fcntl::OFlag::O_WRONLY | nix::fcntl::OFlag::O_CLOEXEC | nix::fcntl::OFlag::O_TRUNC,
                 nix::sys::stat::Mode::from_bits(0o644).unwrap(),
             )?;
-            Self::write_nameserver(fd)?;
+            Self::write_nameserver(unsafe { BorrowedFd::borrow_raw(fd) })?;
         } else {
             self.unmount_resolvconf = true;
         }
@@ -263,7 +263,7 @@ impl Setup {
             self.add_tunnel_routes()?;
 
             // Signal to child that we are done setting up everything.
-            if nix::unistd::write(write_to_parent, &[1])? != 1 {
+            if nix::unistd::write(unsafe { BorrowedFd::borrow_raw(write_to_parent) }, &[1])? != 1 {
                 return Err("Failed to write to pipe".into());
             }
             nix::unistd::close(write_to_parent)?;
@@ -275,7 +275,7 @@ impl Setup {
             mask.add(nix::sys::signal::SIGQUIT);
             mask.thread_block().unwrap();
 
-            let mut fd = nix::sys::signalfd::SignalFd::new(&mask).unwrap();
+            let fd = nix::sys::signalfd::SignalFd::new(&mask).unwrap();
             loop {
                 let res = fd.read_signal().unwrap().unwrap();
                 let signo = nix::sys::signal::Signal::try_from(res.ssi_signo as i32).unwrap();
@@ -311,17 +311,17 @@ impl Setup {
         match fork::fork() {
             Ok(Fork::Child) => {
                 prctl::set_death_signal(nix::sys::signal::SIGINT as isize).unwrap();
-                self.setup_and_handle_signals(read_from_child, write_to_parent);
+                self.setup_and_handle_signals(read_from_child.into_raw_fd(), write_to_parent.into_raw_fd());
                 std::process::exit(0);
             }
             Ok(Fork::Parent(child)) => {
                 self.child = child;
-                nix::unistd::close(write_to_parent)?;
+                nix::unistd::close(write_to_parent.into_raw_fd())?;
                 let mut buf = [0];
-                if nix::unistd::read(read_from_child, &mut buf)? != 1 {
+                if nix::unistd::read(read_from_child.as_raw_fd(), &mut buf)? != 1 {
                     return Err("Failed to read from pipe".into());
                 }
-                nix::unistd::close(read_from_child)?;
+                nix::unistd::close(read_from_child.into_raw_fd())?;
 
                 Ok(())
             }
